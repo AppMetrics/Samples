a1011c2b5e251bd05c6cbf108612b33ea6b8d1eb
al_hardy@live.com.au <al_hardy@live.com.au>
2017-01-11 20:23:01 +1100

    adding influxdb reporting to sample web api 352 project plus sample grafana dashboards for net 452 webapi and mvc

--- src/Web.Api.Net452/Startup.cs
+++ src/Web.Api.Net452/Startup.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Reflection;
+using System.Web.Hosting;
 using System.Web.Http;
 using App.Metrics;
 using App.Metrics.Extensions.Owin.WebApi;
+using App.Metrics.Extensions.Reporting.InfluxDB;
+using App.Metrics.Extensions.Reporting.InfluxDB.Client;
+using App.Metrics.Reporting.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Owin;
 using Owin;
 using Web.Api.Net452;
@@ -30,6 +35,14 @@ namespace Web.Api.Net452
 
             appBuilder.UseMetrics(provider);
 
+            HostingEnvironment.QueueBackgroundWorkItem(cancellationToken =>
+            {
+                var reportFactory = provider.GetRequiredService<IReportFactory>();
+                var metrics = provider.GetRequiredService<IMetrics>();
+                var reporter = reportFactory.CreateReporter();
+                reporter.RunReports(metrics, cancellationToken);
+            });
+
             appBuilder.UseWebApi(httpConfiguration);            
         }
 
@@ -44,7 +57,21 @@ namespace Web.Api.Net452
                 {
                     options.DefaultContextLabel = "Api.Sample.Net452";
                     options.ReportingEnabled = true;
-                }, Assembly.GetExecutingAssembly().GetName())               
+                }, Assembly.GetExecutingAssembly().GetName())
+                 .AddReporting(factory =>
+                 {
+                     factory.AddInfluxDb(new InfluxDBReporterSettings
+                     {
+                         HttpPolicy = new HttpPolicy
+                         {
+                             FailuresBeforeBackoff = 3,
+                             BackoffPeriod = TimeSpan.FromSeconds(30),
+                             Timeout = TimeSpan.FromSeconds(3)
+                         },
+                         InfluxDbSettings = new InfluxDBSettings("appmetricswebapisample452", new Uri("http://127.0.0.1:8086")),
+                         ReportInterval = TimeSpan.FromSeconds(5)
+                     });
+                 })
                 .AddHealthChecks(factory =>
                 {
                     factory.RegisterProcessPrivateMemorySizeHealthCheck("Private Memory Size", 200);
